b35daa42c6e4f17816cbadbc12fbe9d991e488e5
SanzianaTudose <tudosesanziana@gmail.com>
2023-01-04 15:13:13 +0100

    Test Framework Set-up

--- Hello OpenGL/src/Application.cpp
+++ Hello OpenGL/src/Application.cpp
@@ -20,6 +20,8 @@
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_glfw_gl3.h"
 
+#include "tests/TestClearColor.h"
+
 int main(void)
 {
     GLFWwindow* window;
@@ -48,101 +50,37 @@ int main(void)
         std::cout << "Error" << std::endl;
     
     GLCall(std::cout << glGetString(GL_VERSION) << std::endl);
+
     {
+        // Enable alpha blending and specify behavior
         GLCall(glEnable(GL_BLEND));
-        GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)); // Specify how to blend alpha
-
-        float vertexData[] = {
-            -50.0f, -50.0f, 0.0f, 0.0f,
-             50.0f, -50.0f, 1.0f, 0.0f,
-             50.0f,  50.0f, 1.0f, 1.0f,
-            -50.0f,  50.0f, 0.0f, 1.0f
-        };
-        unsigned int indices[] = {
-            0, 1, 2,
-            2, 3, 0
-        };
-
-        VertexArray va;
-        VertexBuffer vb(vertexData, 4 * 4 * sizeof(float));
-        VertexBufferLayout layout;
-        layout.Push<float>(2);
-        layout.Push<float>(2);
-        va.AddBuffer(vb, layout);
-        
-        IndexBuffer ib(indices, 6);
-
-        // Model View Projection Matrices
-        glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f)); // View Matrix = camera position
-
-        Shader shader("res/shaders/Basic.shader");
-
-        Texture texture("res/textures/OpenGL-logo.png");
-        texture.Bind();
-
-        shader.Bind();
-        shader.SetUniform1i("u_Texture", 0);
-
-        Renderer renderer;
+        GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)); 
 
         // ImGui Set-up
         ImGui::CreateContext();
         ImGui_ImplGlfwGL3_Init(window, true);
-        ImGui::StyleColorsDark(); 
-        
-        glm::vec3 translationA(960.0f / 2.0f, 540.0f / 2.0f, 0.0f);
-        glm::vec3 translationB(960.0f / 2.0f, 540.0f / 1.3f, 0.0f);
-        float r = 0.0f;
-        float rIncrement = 0.005f;
-        /* Loop until the user closes the window */
+        ImGui::StyleColorsDark();
+
+        Renderer renderer;
+
+        test::TestClearColor test;
+
         while (!glfwWindowShouldClose(window)) {
             renderer.Clear();
+
+            test.OnUpdate(0.0f);
+            test.OnRender();
+
             ImGui_ImplGlfwGL3_NewFrame();
-            
-            // Draw 1st element
-            {
-                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA); // Model Matrix = model transform (Translation/Rotation/Scale)
-                glm::mat4 mvp = proj * view * model;
-                shader.Bind();
-                shader.SetUniformMat4f("u_MVP", mvp);
-                renderer.Draw(va, ib, shader); 
-            }
-            
-            // Draw 2nd element
-            {
-                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB); // Model Matrix = model transform (Translation/Rotation/Scale)
-                glm::mat4 mvp = proj * view * model;
-                shader.Bind();
-                shader.SetUniformMat4f("u_MVP", mvp);
-                renderer.Draw(va, ib, shader); 
-            }
 
-            if (r > 1.0f)
-                rIncrement *= (-1.0f);
-            else if (r < 0.0f)
-                rIncrement *= (-1.0f);
-
-            r += rIncrement;
-
-            // Render ImGui Debug Window
-            {
-                ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);
-                ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);
-                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            }
+            test.OnImGuiRender();
 
             ImGui::Render();
             ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
 
-            /* Swap front and back buffers */
             glfwSwapBuffers(window);
-
-            /* Poll for and process events */
             glfwPollEvents();
         }
-
-        shader.Unbind();
     }
 
     ImGui_ImplGlfwGL3_Shutdown();
